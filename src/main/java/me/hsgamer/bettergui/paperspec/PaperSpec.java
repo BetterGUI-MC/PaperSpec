035c7e23caf924df888df6b92319e2801d74e42c
Akiranya <porridgen@gmail.com>
2023-01-11 09:12:27 +0800

    Simplify code & make it more safe

--- src/main/java/me/hsgamer/bettergui/paperspec/PaperSpec.java
+++ src/main/java/me/hsgamer/bettergui/paperspec/PaperSpec.java
@@ -6,19 +6,17 @@ import me.hsgamer.bettergui.paperspec.modifier.AdventureLoreModifier;
 import me.hsgamer.bettergui.paperspec.modifier.AdventureNameModifier;
 import me.hsgamer.bettergui.paperspec.modifier.PaperSkullModifier;
 import me.hsgamer.bettergui.paperspec.util.AdventureUtils;
-import me.hsgamer.bettergui.util.MapUtil;
 import me.hsgamer.hscore.bukkit.addon.PluginAddon;
 import me.hsgamer.hscore.bukkit.gui.GUIHolder;
 import me.hsgamer.hscore.bukkit.gui.GUIUtils;
 import me.hsgamer.hscore.common.Validate;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.InventoryHolder;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public final class PaperSpec extends PluginAddon {
     @Override
@@ -43,20 +41,20 @@ public final class PaperSpec extends PluginAddon {
         ItemModifierBuilder.INSTANCE.register(AdventureNameModifier::new, "mini-name", "name$");
         ItemModifierBuilder.INSTANCE.register(AdventureLoreModifier::new, "mini-lore", "lore$");
 
-        InventoryBuilder.INSTANCE.register(pair -> {
-            Map<String, Object> map = pair.getValue();
-            String title = Optional.ofNullable(MapUtil.getIfFound(map, "mini-title", "title$"))
-                .map(String::valueOf)
-                .orElse("");
-            return (display, uuid) -> {
-                GUIHolder holder = display.getHolder();
-                InventoryType type = holder.getInventoryType();
-                int size = holder.getSize(uuid);
-                Component adventure$title = AdventureUtils.toComponent(title);
-                return type == InventoryType.CHEST && size > 0
-                       ? Bukkit.createInventory(display, GUIUtils.normalizeToChestSize(size), adventure$title)
-                       : Bukkit.createInventory(display, type, adventure$title);
-            };
+        InventoryBuilder.INSTANCE.register(pair -> (display, uuid) -> {
+            GUIHolder holder = display.getHolder();
+            InventoryType type = holder.getInventoryType();
+            int size = holder.getSize(uuid);
+
+            // MiniMessage deserializer will throw a runtime exception
+            // if the string contains any legacy color codes (&1, &b).
+            // So we strip it in case the menu fails to be loaded.
+            String title = ChatColor.stripColor(holder.getTitle(uuid));
+
+            Component adventure$title = AdventureUtils.toComponent(title);
+            return type == InventoryType.CHEST && size > 0
+                   ? Bukkit.createInventory(display, GUIUtils.normalizeToChestSize(size), adventure$title)
+                   : Bukkit.createInventory(display, type, adventure$title);
         }, "mini-title");
     }
 }
